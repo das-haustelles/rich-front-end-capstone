3b34290311cdb979540be9e72dfbf606e8d019ed
Richard Huynh <richard.c.huynh@gmail.com>
2019-04-03 11:15:16 -0700

    Cleanup style errors for seeding utility functions

--- db/utils/bookings.js
+++ db/utils/bookings.js
@@ -2,16 +2,16 @@ const faker = require('faker');
 
 const generateBookings = (hostels, desiredBookings) => {
   const bookings = [];
-  for(var i = 1; i <= hostels; i++) {
-    for (var x = 0; x < desiredBookings; x++) {
+  for (let i = 1; i <= hostels; i += 1) {
+    for (let x = 0; x < desiredBookings; x += 1) {
       const booking = {
         hostel_id: i,
         bookedDate: faker.date.future(1, new Date()),
-      }
+      };
       bookings.push(booking);
     }
   }
   return bookings;
-}
+};
 
 module.exports.generateBookings = generateBookings;
--- db/utils/hostels.js
+++ db/utils/hostels.js
@@ -2,14 +2,14 @@ const faker = require('faker');
 
 const generateHostels = (desiredFakeHostels) => {
   const hostels = [];
-  for (var i = 0; i < desiredFakeHostels; i++) {
+  for (let i = 0; i < desiredFakeHostels; i += 1) {
     const hostel = {
       hostelName: faker.company.companyName(),
       roomName: faker.commerce.productName(),
-      roomDescription: faker.lorem.sentences(3), 
-      roomPrice: faker.commerce.price(14, 115)
-    }
-    hostels.push(hostel); 
+      roomDescription: faker.lorem.sentences(3),
+      roomPrice: faker.commerce.price(14, 115),
+    };
+    hostels.push(hostel);
   }
   return hostels;
 };
