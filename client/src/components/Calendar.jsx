21d1aa64ca166cc1788b89b06e60630cfa06bc5f
Richard Huynh <richard.c.huynh@gmail.com>
2019-04-03 11:01:49 -0700

    Install moment for calendar module

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import Dates from './Dates';
+import moment from 'moment';
 
 class Calendar extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      month: 0,
+      year: 0,
+      firstDay: 0,
+    }
+  }
+
+  componentDidMount() {
+    const currentDate = moment();
+    this.setState({
+      month: currentDate.month(),
+      year: currentDate.year(),
+      firstDay: currentDate.day(),
+    });
+  }
+
+  getFirstDayOfMonth() {
+    const firstDay = new Date(this.state.year, this.state.month, 1);
+    this.setState({firstDay: firstDay});
   }
 
   render() {
@@ -10,14 +31,7 @@ class Calendar extends React.Component {
 
     return(
       <div>
-        <table>
-          <th>
-            <tr>
-            {weekdays.map((day) => (<td id= {day}> {day}</td>))}
-            </tr>
-          </th>
-          <tbody> </tbody>
-        </table>
+        
       </div>
     )
   }
@@ -28,3 +42,7 @@ export default Calendar;
 // Calendar is a 7 x 7 table
   // Table header is static with weekday abbrieviations
   // Table body is dynamic and has 6 rows and 6 columns
+
+// Date Object
+//// 0 - 11 represents month of the year
+//// 0 - 30/31 represents day of the month
