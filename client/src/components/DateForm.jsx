a18855593cedb2d8e7c3f97caa069548acb29e5e
Richard Huynh <richard.c.huynh@gmail.com>
2019-04-04 13:39:16 -0700

    Convert DateForm to class

--- client/src/components/DateForm.jsx
+++ client/src/components/DateForm.jsx
@@ -3,22 +3,30 @@ import CheckInInput from './CheckInInput';
 import CheckOutInput from './CheckOutInput';
 import GuestsDropDown from './GuestsDropDown';
 
-const DateForm = ({ checkIn, checkOut, bookedDates }) => {
-  return(
-    <div>
-      <form>
-        <span>
-          <CheckInInput checkIn={checkIn} />
-          <CheckOutInput checkOut={checkOut}/>
-          <GuestsDropDown />
-        </span>
-        <div className="search-submit-row">
-          <span className="form-label"></span>
-          <button type="submit" className="search-button">Search</button>
-        </div>
-      </form>
-    </div>
-  )
+class DateForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      guests: 0
+    }
+  }
+  
+  render() {
+    return(
+      <div>
+        <form>
+          <span>
+            <CheckInInput checkIn={checkIn} />
+            <CheckOutInput checkOut={checkOut}/>
+            <GuestsDropDown />
+          </span>
+          <div className="search-submit-row">
+            <span className="form-label"></span>
+            <button type="submit" className="search-button">Search</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
-
 export default DateForm;
