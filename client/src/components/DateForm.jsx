55fe823c6754cb0ae592ae67d8a3a2bcee7c5dba
Richard Huynh <richard.c.huynh@gmail.com>
2019-04-04 14:20:32 -0700

    Render group form if guests larger than nine

--- client/src/components/DateForm.jsx
+++ client/src/components/DateForm.jsx
@@ -2,23 +2,25 @@ import React from 'react';
 import CheckInInput from './CheckInInput';
 import CheckOutInput from './CheckOutInput';
 import GuestsDropDown from './GuestsDropDown';
+import GroupForm from './GroupForm';
 
 class DateForm extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      guests: 0
+      guests: 9
     }
   }
-  
+
   render() {
+    const largeParty = this.state.guests >= 9;
     return(
       <div>
         <form>
           <span>
-            <CheckInInput checkIn={checkIn} />
-            <CheckOutInput checkOut={checkOut}/>
-            <GuestsDropDown />
+            <CheckInInput checkIn={this.props.checkIn} />
+            <CheckOutInput checkOut={this.props.checkOut}/>
+            {largeParty ? <div> <GuestsDropDown /> <GroupForm /></div> : <GuestsDropDown />}
           </span>
           <div className="search-submit-row">
             <span className="form-label"></span>
